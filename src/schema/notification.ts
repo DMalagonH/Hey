388a83e90c45b2bcec52393d6a290395fdc2efc5
Diego Malagón <diegomalagonh@gmail.com>
2017-11-21 12:49:42 -0500

    Notification schema and code fits

---
+++ src/schema/notification.ts
@@ -0,0 +1,12 @@
+export class Notification{
+	id:number;
+	appName:string;
+	appKey:string;
+	title:string;
+	body:string;
+	type:string;
+	datetime:string;
+	url:string;
+	viewed:boolean;
+	archived:boolean;
+}
--- src/services/notifications.service.ts
+++ src/services/notifications.service.ts
@@ -3,17 +3,9 @@ import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class NotificationsService{
-	notifications = [
-		{ title: 'Notification 4', body:'N4 Donec maximus justo ac sapien ullamcorper.', type: "info", appName: "Hey", appKey: "123", viewed: false, archived: false, datetime: '6:00 am 17/11/17', url:'https://github.com/DMalagonH'},
-		{ title: 'Notification 5', body:'N5 Donec maximus justo ac sapien ullamcorper.', type: "info", appName: "Hey", appKey: "123", viewed: false, archived: false, datetime: '8:30 pm 16/11/17', url:'https://github.com/DMalagonH'},
-		{ title: 'Notification 6', body:'N6 Donec maximus justo ac sapien ullamcorper.', type: "info", appName: "Hey", appKey: "123", viewed: false, archived: false, datetime: '6:00 pm 16/11/17', url:'https://github.com/DMalagonH'}
-	];
+	public firebase_path:string = 'notifications/';
 
-	firebase_path = 'notifications/';
-
-	constructor(public afDB: AngularFireDatabase){
-
-	}
+	constructor(public afDB: AngularFireDatabase){}
 
 	public getAll() {
 		return this.afDB.list(this.firebase_path);
@@ -25,13 +17,5 @@ export class NotificationsService{
 
 	public remove(notification) {
 		this.afDB.database.ref(this.firebase_path+notification.id).remove();
-	}
-
-	public upload(){
-		let self = this;
-		this.notifications.forEach((n) => {
-			let id = Date.now();
-			this.afDB.database.ref(this.firebase_path + id).set(n);
-		})
 	}
 }
